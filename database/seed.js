05053cada7490dddfefdac71a3f8ec5264abcd02
spencerng94 <spencerng94@gmail.com>
2020-06-19 15:05:06 -0700

    Added seeding script for mainProduct and photos

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,6 @@
 const db  = require('./index.js');
-const mainProduct = require('./mainProduct.js');
-const photos = require('./mainProduct.js')
+const {mainProduct} = require('./mainProduct.js');
+const {photos} = require('./mainProduct.js')
 var faker = require('faker');
 const mongoose = require('mongoose');
 var async = require('async');
@@ -8,14 +8,6 @@ var Schema = mongoose.Schema;
 
 // MAIN PRODUCT FAKE DATA
 
-let randomCategory = faker.commerce.department(); // Clothing
-let randomCompany = faker.company.companyName(); // Apple Inc.
-let randomDescription = faker.image.abstract(); // image description
-let randomPrice = faker.commerce.price(); // $1.00
-let randomPrime = faker.random.boolean(); // true
-let randomRating = faker.random.number(); // 4
-let randomTitle = faker.commerce.productName(); // Swimming Pool
-let randomBestSeller = faker.random.boolean(); // false
 
 
 // PHOTOS FAKE DATA
@@ -25,47 +17,55 @@ var randomPhotoUrl = faker.image.imageUrl(); // https://aws.photo.com
 // seed main product table
 
 let seedMainProduct = function () {
-    return new Promise (function (resolve, reject) {
+    var promisearr = [];
 
         for (var i = 0; i < 100; i++) {
-            
-            function Product(category, company, description, price, prime, rating, title, bestseller) {
-                this.category = randomCategory;
-                this.company = randomCompany;
-                this.description = randomDescription;
-                this.price = randomPrice;
-                this.prime = randomPrime;
-                this.rating = randomRating;
-                this.title = randomTitle;
-                this.bestseller = randomBestSeller;
-            }
 
-            const products = new Product(randomCategory, randomCompany, randomDescription, randomPrice, randomPrice, randomRating, randomTitle, randomBestSeller)
+            let randomCategory = faker.commerce.department(); // Clothing
+            let randomCompany = faker.company.companyName(); // Apple Inc.
+            let randomDescription = faker.image.abstract(); // image description
+            let randomPrice = faker.commerce.price(); // $1.00
+            let randomPrime = faker.random.boolean(); // true
+            let randomRating = faker.random.number(); // 4
+            let randomTitle = faker.commerce.productName(); // Swimming Pool
+            let randomBestSeller = faker.random.boolean(); // false
 
-            let unit = new mainProduct({
-                category: `${products.randomCategory}`,
-                company: `${products.randomCompany}`,
-                description: `${products.randomDescription}`,
-                price: `${products.randomPrice}`,
-                prime: `${products.randomPrime}`,
-                ratings: `${products.randomRating}`,
-                title: `${products.randomTitle}`,
-                best_seller: `${products.randomBestSeller}`
-            }).save(function (err) {
-                if(err) {
-                    console.log(err, 'err from .save')
-                    reject
-                } else {
-                    resolve();
-                }
-            })
+            let mainproduct = new mainProduct({
+                category: `${randomCategory}`,
+                company: `${randomCompany}`,
+                description: `${randomDescription}`,
+                price: `${randomPrice}`,
+                prime: `${randomPrime}`,
+                ratings: `${randomRating}`,
+                title: `${randomTitle}`,
+                best_seller: `${randomBestSeller}`
+            }).save()
+            promisearr.push(mainproduct)
         }
-
-    });
+    return Promise.all(promisearr).catch(err => console.log(err))
 };
 
+let seedPhotos = function() {
+    var promisearr = [];
 
+    for (var j = 0; j < 100; j++) {
+        let randomPhotoUrl = faker.image.imageUrl(); // https://www.aws.photo.com
 
+        let photostable = new photos({
+            photo_url: `${randomPhotoUrl}`
+        }).save()
+        promisearr.push(photostable)
+    }
+    return Promise.all(promisearr).catch(err => console.log(err, 'error from seedPhotos'))
+}
+
+mainProduct.db.dropDatabase(function(err, results) {
+    if(err) {
+        console.log(err, 'error from dropdb')
+    } else {
+        console.log(results, 'results from dropdb')
+    }
+})
 
 const insertSampleProducts = function() {
   mainProduct.create(seedMainProduct())
@@ -75,6 +75,11 @@ const insertSampleProducts = function() {
 
 insertSampleProducts();
 
+const insertSamplePhotos = function() {
+    photos.insert(seedPhotos())
+        .then(() => db.close())
+        .catch(err => console.log(err, 'err from insertSamplePhotos'))
+}
 
-
+insertSamplePhotos()
 // seed photos table
