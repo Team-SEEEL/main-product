7aa2aeebe559f657b149088a1286729a0986ca85
spencerng94 <spencerng94@gmail.com>
2020-09-08 18:53:29 -0700

    deployment trial

--- test/test.spec.js
+++ test/test.spec.js
@@ -4,14 +4,25 @@ import {
   shallow, mount, render, configure,
 } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from '../client/src/App';
+import App from '../client/src/components/App';
 
-describe('Api test', () => {
-  test('Ajax request to endpoint /api/products should return data', () => {
-    return axios.get('http://localhost:3001/products/api/mainProduct')
-      .then((results) => {
-        expect(results.data).toHaveLength(100);
-      });
-  });
+describe('<App /> rendering', () => {
+    it('should render three <div>s', () => {
+      let wrapper = shallow(<App />);
+      expect(wrapper.children('div')).toHaveLength(3);
+    });
 });
 
+configure({ adapter: new Adapter() });
+
+describe('A suite', function () {
+    it('Should return a div element', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.type()).toEqual('div');
+    })
+
+    it('Should render correctly', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
